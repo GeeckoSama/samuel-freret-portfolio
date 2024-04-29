918c49c823c37fdb0cf9873aac1ed20cb67f97d2
GeeckoSama <julienheller81@gmail.com>
2024-04-29 15:18:40 +0200

    Ajouter le package @astrojs/vercel à package.json et configurer l'adaptateur Vercel dans astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import { imageService } from "@unpic/astro/service";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
   image: {
-    service: imageService(),
+    service: imageService()
   },
+  adapter: vercel()
 });
