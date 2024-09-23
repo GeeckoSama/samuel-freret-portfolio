896e5c62c62fb680223defe7dbd8bd094001b122
GeeckoSama <julienheller81@gmail.com>
2024-09-23 16:57:28 +0200

    Add seo & robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { imageService } from "@unpic/astro/service";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  site: "https://guiltyvision.com",
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) =>
+        page !== "https://guiltyvision.com/signin" &&
+        "https://guiltyvision.com/admin",
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date("2024-09-23"),
+    }),
+  ],
   image: {
     service: imageService(),
   },
