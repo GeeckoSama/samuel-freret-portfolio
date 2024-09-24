f5ccd8105282ecce7130f70309a14e718c6a96c1
GeeckoSama <julienheller81@gmail.com>
2024-09-24 08:11:15 +0200

    modification du theme daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,18 +4,40 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        base: { light: "#F7F7F7", dark: "#1A1A1A" },
-        primary: { light: "#1A1A1A", dark: "#F7F7F7" },
-        accent: { light: "#b5e3d8", dark: "#89c1b2" },
-        danger: { light: "#ffa4a4", dark: "#ff8c8c" },
-        success: { light: "#98e5b0", dark: "#7bc79c" },
-      },
       fontFamily: {
         sans: ["CabinetGrotesk-Variable", "sans-serif"],
         serif: ["Merriweather", "serif"],
       },
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#1a1a1a",
+          "primary-content": "#cbcbcb",
+          secondary: "#a5f3fc",
+          "secondary-content": "#0a1416",
+          accent: "#a7f3d0",
+          "accent-content": "#0a1410",
+          neutral: "#1a1a1a",
+          "neutral-content": "#cbcbcb",
+          "base-100": "#f7f7f7",
+          "base-200": "#d7d7d7",
+          "base-300": "#b7b7b7",
+          "base-content": "#151515",
+          info: "#a5f3fc",
+          "info-content": "#0a1416",
+          success: "#4ade80",
+          "success-content": "#021206",
+          warning: "#fbbf24",
+          "warning-content": "#150d00",
+          error: "#ef4444",
+          "error-content": "#140202",
+        },
+      },
+      "dark",
+    ],
   },
   plugins: [daisyui],
 };
